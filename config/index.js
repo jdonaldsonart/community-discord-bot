f5e06b1cb13c4153131e7cd4796a21259ca303a5
Josh P. Sawyer <josh.p.sawyer@gmail.com>
2022-03-26 10:33:15 -0400

    parse exitoncaughtexception

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 exports.bot = {
     token: process.env.DISCORD_BOT_TOKEN || "",
     botName: process.env.DISCORD_BOT_NAME || "Discord Bot",
+    exitOnUncaughtException: JSON.parse(process.env.DISCORD_BOT_EXIT_EXCEPTION) || true,
     threadFromImageChannels: JSON.parse(process.env.THREAD_FROM_IMAGE_CHANNELS) || [],
     noGifChannels: JSON.parse(process.env.NO_GIF_CHANNELS) || []
 }
